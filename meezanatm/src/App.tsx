5f56c401d20fe20631c1a6f24b6584d44a0b9344
samade747 <samad.e747@gmail.com>
2024-03-26 22:00:26 +0500

    10pm 26 3 2024 meezan atm updating

---
+++ meezanatm/src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import React from 'react'
+import './App.css'
+import Meezanatm from './Components/meezanatm'
+
+function App() {
+  
+
+  return (
+    <>
+    <Meezanatm />
+    </>
+  )
+}
+
+export default App
---
+++ meezanatm/src/Components/Meezanatm.tsx
@@ -0,0 +1,23 @@
+import inquirer from "inquirer"
+
+interface UserType {
+    userId: number,
+    pin: number,
+    transactionId: string,
+    amount?: number
+}
+
+
+
+
+
+
+// export default function Meezanatm() {
+//     return (
+//         <div className="App">
+//             <header className="App-header">
+//                 <h1>Meezan ATM</h1>
+//             </header>
+//         </div>
+//     )
+// }
