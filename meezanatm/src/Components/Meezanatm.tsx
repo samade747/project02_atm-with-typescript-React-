2f2fa76d3c88ffb72fbd6525cdf5d4e719aa7e87
samade747 <samad.e747@gmail.com>
2024-03-27 04:49:32 +0500

    449am 27 3 2024

--- meezanatm/src/Components/Meezanatm.tsx
+++ meezanatm/src/Components/Meezanatm.tsx
@@ -7,17 +7,46 @@ interface UserType {
     amount?: number
 }
 
-async function  UseAtm(){
+export default async function UseAtm(){
 
     const userInput: UserType = await inquirer.prompt([
         {
-            type: "input",
-            name: "userId",
-            message: "Please Enter Your User Id: ",
-            validate: (value) => {
-                if (value === "") {
+            type: "password",
+            name: "pin",
+            message: "Enter Your Pin:"
+        },
+        {
+            type: "list",
+            name: "accountType",
+            choices: ["Saving", "Current"],
+            message: "Select Account Type:"
+        },
+        {
+            type: "list",
+            name: "transactionType",
+            choices: ["Withdraw", "Deposit", "Balance Enquiry", "Transfer", "Mini Statement", "Change Pin", "Exit"],
+            message: "Select Transaction Type:"
+        },
+        {
+            type: "number",
+            name: "amount",
+            message: "Enter Amount:",
+            when: (userInput: UserType) => userInput.transactionType === "Withdrawal"
+        },
+        {
+            type: "list",
+            name: fastCashAmount",
+            message:  choices: ["500", "1000", "2000", "3000", "5000", "10000"],
+            when: (userInput: UserType) => userInput.transactionType === "Fast Cash",
+
         }
-    ])    
+
+
+    ]);
+
+
+
+
 }
 
 
